49e5cbfac10c12f9bef246b27eb84be63a4804b8
Keith Marshall <code@eleusis.co.uk>
2014-01-28 17:44:16 +0000

    Change user.js to use promises version of deviceUtils

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@
  */
 
 var db = require('../mongodb');
+var deviceUtils = require('../lib/deviceUtils');
 
 exports.list = function(req, res){
   res.send("respond with a resource");
@@ -11,117 +12,117 @@ exports.list = function(req, res){
 
 exports.register = function(req, res)
 {
-	var regid = req.body.regid;
-	
-	if (!regid || regid.length == 0)
+	var regid = getRegidFromRequest(req, res);
+	if (regid)
 	{
-		res.status(400).send("No registration ID!");
-		return;
+		// check if it exists already, otherwise this will give an error!
+		// If it does exist, just leave it as-is - the user probably updated
+		// to a new version of the app which re-registered with the same ID.
+		db.devices.findOne({'_id': regid}, function(err, found) {
+			if (!found) 
+			{
+				deviceUtils.insertDevice({"_id": regid}).then(
+					function(saved) {
+						res.send(saved);
+					}, 
+					function(err) {
+						res.status(500).send("An error occurred: " + err);
+					}
+				);
+			}
+			else
+			{
+				res.send(found);
+			}
+		});
 	}
-	
-	// check if it exists already, otherwise this will give an error!
-	db.devices.find({'_id': regid}, function(err, list){
-		if (list.length == 0) {
-			db.devices.insert({"_id": regid}, function(err, saved) {
-				if (err || !saved)
-				{
-					res.status(500).send("An error occurred: " + err);
-				}
-				else
-				{
-					res.send(saved);
-				}
-			});
-		}
-		else
-		{
-			res.send(list[0]);
-		}
-	})
 };
 
 exports.deregister = function(req, res)
 {
-	var regid = req.body.regid;
-	
-	if (!regid || regid.length == 0)
+	var regid = getRegidFromRequest(req, res);
+	if (regid)
 	{
-		res.status(400).send("No registration ID!");
-		return ;
+		// Remove device regid from mongo:
+		deviceUtils.deleteDevice(regid).then(
+			function() {
+				res.send("Item removed.");
+			},
+			function(err) {
+				res.status(500).send("An error occurred, item not removed: " + err);
+			}
+		);
 	}
-	
-	// Remove device regid from mongo:
-	db.devices.remove({"_id": regid}, function(err) {
-		if (err)
-		{
-			res.status(500).send("An error occurred, item not removed: " + err);
-		}
-		else
-		{
-			res.send("Item removed.");
-		}
-	});
 };
 
 exports.setCountries = function(req, res)
 {
-	var regid = req.body.regid;
-	var countriesJson = req.body.countries;
-	
-	if(!regid || regid.length == 0 || !countriesJson)
+	var regid = getRegidFromRequest(req, res);
+	if (regid)
 	{
-		res.status(400).send("No regid, or missing countries field!");
-		return;
-	}
+		var countriesJson = req.body.countries;
 	
-	var countries = JSON.parse(countriesJson);
-	var device = {
+		var countries = JSON.parse(countriesJson);
+		var device = {
 			"_id": regid,
 			"countries": countries
-	};
-	
-	db.devices.update({"_id": regid}, device, {upsert: true}, function(err) {
-		if(err)
-		{
-			res.status(500).send("An error occurred updating settings: " + err);
-		}
-		else
-		{
-			res.send("Updated.");
-		}
-	});
+		};
+		
+		deviceUtils.updateDevice(device).then(
+			function() {
+				res.send("Updated.");
+			},
+			function(err) {
+				res.status(500).send("An error occurred updating settings: " + err);
+			}
+		);
+	}
 };
 
-exports.removeCountries = function(req, res) {
+exports.removeCountries = function(req, res) 
+{
+	var regid = getRegidFromRequest(req, res);
+	if (regid)
+	{
+		// fetch the device, remove the countries property, 
+		// then update with the changed device object:
+		db.devices.findOne({'_id': regid}, function(err, device) {
+			if (err)
+			{
+				res.status(500).send("An error occurred removing countries: " + err);
+				return;
+			}
+			
+			delete device.countries;
+			deviceUtils.updateDevice(device).then(
+				function() {
+					res.send("Updated.");
+				},
+				function(err) {
+					res.status(500).send(
+						"An error occurred updating device with removed countries: " + err);
+				}
+			);
+			
+		});
+	}
+};
+
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+// INTERNAL FUNCTIONS
+
+function getRegidFromRequest(req, res) 
+{
 	var regid = req.body.regid;
 	
-	if (!regid || regid.length == 0)
+	if (!regid || regid.length === 0)
 	{
 		res.status(400).send("No registration ID!");
-		return ;
+		return null;
 	}
-	
-	// fetch the device, remove the countries property, update with the changed device object:
-	db.devices.findOne({'_id': regid}, function(err, device) {
-		if (err)
-		{
-			res.status(500).send("An error occurred removing countries: " + err);
-			return;
-		}
-		
-		delete device.countries;
-		db.devices.update({"_id": regid}, device, function(err) {
-			if(err)
-			{
-				res.status(500).send(
-						"An error occurred updating device with removed countries: " + err);
-			}
-			else
-			{
-				res.send("Updated.");
-			}
-		});
-		
-	});
-	
-};
+	else
+	{
+		return regid;
+	}
+}
