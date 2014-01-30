d199a598df71841e1cc1854f84f08d63ca61c466
Keith Marshall <code@eleusis.co.uk>
2014-01-30 17:52:24 +0000

    alert feed routing now uses promises

--- src/routes/fco-alert-feed.js
+++ src/routes/fco-alert-feed.js
@@ -2,110 +2,120 @@
  * New node file
  */
 
-var db = require('../lib/mongodb');
+var RSVP = require('rsvp');
 var FeedParser = require("feedparser");
 var request = require('request');
-var globals = require('../lib/globalsDao');
+
 var alerter = require('../lib/alerter');
+var alertDao = require('../lib/alertDao');
+var globals = require('../lib/globalsDao');
 var utils = require('../lib/utils');
 
-var alertDao = require('../lib/alertDao');
-
-function storeAlert(alert)
-{
-	alert._id = alert.guid;
-	for (var key in alert)
-	{
-		// remove all the "atom:" keys - they're duplicates and they just use up space
-		if (key.lastIndexOf("atom") === 0)
-		{
-			delete alert[key];
-		}
-	}
-	db.alerts.update({"_id": alert._id}, alert, {upsert: true});
-}
 
 function updateAlertsFromFeed(feed)
 {
-	// hmm.. this is where async gets  annoying. I wonder if promises would make
-	// for clearer code,
-	globals.lastUpdated().then(function(lastUpdated) {
-		console.log("Feed date: " + feed.meta.pubDate + ", lastupdated = " + lastUpdated);
-		if (feed.meta.pubDate > lastUpdated)
-		{
-			console.log("Updating alerts in database..");
-			for (var i = 0; i < feed.entries.length; i++)
+	return new RSVP.Promise(function(resolve, reject) {
+		globals.lastUpdated().then(function(lastUpdated) {
+			console.log("Feed date: " + feed.meta.pubDate + ", lastupdated = " + lastUpdated);
+			if (feed.meta.pubDate > lastUpdated)
 			{
-				var item = feed.entries[i];
-				if (item.pubDate > lastUpdated)
+				console.log("Updating alerts in database..");
+				var promises = [];
+				for (var i = 0; i < feed.entries.length; i++)
 				{
-					storeAlert(item);
-					item.description = utils.stripHtml(item.description);
-					alerter.triggerAlert(item).then();
+					var item = feed.entries[i];
+					if (item.pubDate > lastUpdated)
+					{
+						promises.push(alertDao.upsertAlert(item).then(function() {
+							item.description = utils.stripHtml(item.description);
+							return alerter.triggerAlert(item);
+						}));
+					}
 				}
+				RSVP.allSettled(promises)
+					.then(function(states) {
+						// TODO: ignoring states for now, may need to log failures later
+						console.log("Finished updating alerts in database.");
+						return globals.setLastUpdated(feed.meta.pubDate);
+					})
+					.then(function() {
+						resolve(feed);
+					})
+					.catch(reject);
 			}
-			console.log("Finished updating alerts in database.");
-			globals.setLastUpdated(feed.meta.pubDate).then();
-		}
-		else
-		{
-			console.log("Not updating, no changes detected");
-		}
-	}).catch(function(err) {
-		console.log("Error updating alerts from feed: " + err);
+			else
+			{
+				console.log("Not updating, no changes detected");
+				resolve(feed);
+			}
+		}).catch(function(err) {
+			console.log("Error updating alerts from feed: ", err);
+			reject(err);
+		});
 	});
 }
 
-function fetchAlerts(onFinish, onError)
+function fetchAlerts()
 {
-	console.log("Fetching alerts..");
-	var feed = { entries: [] };
-	
-	// NB, FCO site doesn't appear to support if-modified-since, so we have to fetch
-	// the whole lot each time.
-	request('https://www.gov.uk/foreign-travel-advice.atom')
-	.on('error', function(error) {
-		if (onError) {onError(error);}
-	})
-	.pipe(new FeedParser({addmeta: false}))
-	.on('error', function(error) {
-		if (onError) {onError(error);}
-	})
-	.on('meta', function (meta) {
-		feed.meta = meta;
-	})
-	.on('readable', function () {
-		var stream = this;
-		var item = stream.read();
-		while (item)
-		{
-			feed.entries.push(item);
-			item = stream.read();
-		}
-	})
-	.on('end', function() {
-		if (onFinish) {onFinish(feed);}
-		console.log("Finished fetching alerts.");
+	return new RSVP.Promise(function(resolve, reject) 
+	{
+		console.log("Fetching alerts..");
+		var feed = { entries: [] };
+		
+		// NB, FCO site doesn't appear to support if-modified-since, so we have to fetch
+		// the whole lot each time.
+		request('https://www.gov.uk/foreign-travel-advice.atom')
+			.on('error', reject) // errors on request
+			.pipe(new FeedParser({addmeta: false}))
+				.on('error', reject) // errors on pipe, I think we need both
+				.on('meta', function (meta) {
+					feed.meta = meta;
+				})
+				.on('readable', function () {
+					var stream = this;
+					var item = stream.read();
+					while (item)
+					{
+						feed.entries.push(item);
+						item = stream.read();
+					}
+				})
+				.on('end', function() {
+					console.log("Finished fetching alerts");
+					resolve(feed);
+				});
 	});
 
 }
 
-function updateAlerts()
+/**
+ * Call this outside of a web environment - returns a promise
+ */
+exports.updateAlerts = function(onFinish)
 {
-	fetchAlerts(updateAlertsFromFeed, null);
-}
-
-
-exports.refresh = function(req, res)
-{
-	fetchAlerts(
-			function(feed) {
-				updateAlertsFromFeed(feed);
-				res.send(feed);
-			},
-			function(error) {
-				res.status(500).send(error);
-			});
+	return new RSVP.Promise(function(resolve, reject) {
+		fetchAlerts()
+			.then(updateAlertsFromFeed)
+			.then(resolve)
+			.catch(reject);
+	});
 };
 
-exports.updateAlerts = updateAlerts;
+
+/**
+ * Call this inside a web environment - ensures a response is always sent.
+ */
+exports.refresh = function(req, res)
+{
+	fetchAlerts()
+		.then(updateAlertsFromFeed)
+		.then(function(feed) {
+			// for some reason, just passing res.send here doesn't work, we need to
+			// create this anonymous function. Dunno why.
+			console.log("placing feed on HTTP response..");
+			res.send(feed);
+		})
+		.catch(function(err) {
+			res.status(500).send(err);
+		});
+};
