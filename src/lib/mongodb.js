63a3e68fd11d5e12b21f80c3b1e12601844896dd
Keith Marshall <code@eleusis.co.uk>
2016-11-05 14:19:10 +0000

    Update mongojs package, allow new env var to override MONGOHQ

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -6,10 +6,17 @@
  * var mongodb = require("./mongodb");
  */
 
-var databaseURI = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||
+var databaseURI = process.env.FCOALERTS_MONGO_URI ||
+    process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||
 	process.env.MONGOTEST_URL ||"localhost:27017/fco-alerts";
 var collections = ["alerts", "devices", "meta", "worldgeo", "retireddevices"];
-var mongodb = require("mongojs").connect(databaseURI, collections);
+
+// Old version for mongojs v0.x
+// var mongodb = require("mongojs").connect(databaseURI, collections);
+
+// New version for mongojs 1.x and beyond
+var mongojs = require("mongojs");
+var mongodb = mongojs(databaseURI, collections, {authMechanism: 'SCRAM-SHA-1'});
 
 mongodb.alerts.ensureIndex({'title': 1});
 mongodb.devices.ensureIndex({'countries': 1});
